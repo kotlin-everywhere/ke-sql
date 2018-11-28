6e1208c25fdc568a28fb7e4916a0b1b915b4f943
Park Sung Min <vamf12@gmail.com>
2018-11-28 20:10:02 +0900

    add password option

--- src/main/kotlin/com/minek/kotlin/everywhere/ke/sql/engine.kt
+++ src/main/kotlin/com/minek/kotlin/everywhere/ke/sql/engine.kt
@@ -3,12 +3,13 @@ package com.minek.kotlin.everywhere.ke.sql
 import io.reactiverse.pgclient.PgClient
 import io.reactiverse.pgclient.PgPoolOptions
 
-class Engine(database: String, user: String, host: String? = null, maxSize: Int = 5, sendBufferSize: Int? = null) {
+class Engine(database: String, user: String, password: String? = null, host: String? = null, maxSize: Int = 5, sendBufferSize: Int? = null) {
     private val client = PgPoolOptions()
             .run { if (host != null) setHost(host) else this }
             .setPort(5432)
             .setDatabase(database)
             .setUser(user)
+            .run { if (password != null) setPassword(password) else this }
             .setMaxSize(maxSize)
             .run { if (sendBufferSize != null) setSendBufferSize(sendBufferSize) else this }
             .let(PgClient::pool)
